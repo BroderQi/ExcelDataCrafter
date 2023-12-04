5a8bc60ff1862ebbee612a3683411fd2846aa672
Broder <1320651178@qq.com>
2023-12-04 21:30:08 +0800

    Initial commit

---
+++ edc.web/config/routes.ts
@@ -0,0 +1,70 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        name: 'login',
+        path: '/user/login',
+        component: './user/Login',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/excel',
+    name: 'Excel',
+    icon: 'crown',
+    routes: [
+      {
+        path: '/excel/excel-import',
+        name: '导入向导',
+        icon: 'smile',
+        component: './excel/excel-import'
+      },
+      {
+        component: './404',
+      },
+    ],
+    
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+
+];
