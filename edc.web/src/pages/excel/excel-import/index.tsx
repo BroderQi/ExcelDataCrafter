4e86103bb61fe9a16117fce3f48c8e194bd88539
Broder <1320651178@qq.com>
2023-12-05 16:57:42 +0800

    <feature> Init WebApi

--- edc.web/src/pages/excel/excel-import/index.tsx
+++ edc.web/src/pages/excel/excel-import/index.tsx
@@ -1,6 +1,9 @@
-import { Card, message } from 'antd';
+import { Card, message, Upload } from 'antd';
 import type { FC } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
+import { InboxOutlined } from '@ant-design/icons';
+import type { UploadProps } from 'antd';
+const { Dragger } = Upload;
 import {
   ProCard,
   ProForm,
@@ -22,187 +25,159 @@ const waitTime = (time: number = 100) => {
 };
 
 
+const props: UploadProps = {
+  name: 'file',
+  multiple: true,
+  action: 'https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188',
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log('Dropped files', e.dataTransfer.files);
+  },
+};
 
 const ExcelImport: FC<Record<string, any>> = () => {
+
+
+
   return (
     <PageContainer content="Excel文件导入向导">
-     <>
-      <StepsForm
-        onFinish={async (values) => {
-          console.log(values);
-          await waitTime(1000);
-          message.success('提交成功');
-        }}
-        formProps={{
-          validateMessages: {
-            required: '此项为必填项',
-          },
-        }}
-      >
-        <StepsForm.StepForm
-          name="base"
-          title="第一步骤"
-          onFinish={async () => {
-            await waitTime(2000);
-            return true;
+      <>
+        <StepsForm
+          onFinish={async (values) => {
+            console.log(values);
+            await waitTime(1000);
+            message.success('提交成功');
+          }}
+          formProps={{
+            validateMessages: {
+              required: '此项为必填项',
+            },
           }}
         >
-          <ProCard
-            title="源和目标"
-            bordered
-            headerBordered
-            collapsible
-            style={{
-              marginBlockEnd: 16,
-              minWidth: 800,
-              maxWidth: '100%',
+          <StepsForm.StepForm
+            name="base"
+            title="第一步骤"
+            onFinish={async () => {
+              await waitTime(2000);
+              return true;
             }}
           >
-            <ProFormText
-              name="name"
-              width="md"
-              label="迁移任务名称"
-              tooltip="最长为 24 位，用于标定的唯一 id"
-              placeholder="请输入名称"
-              rules={[{ required: true }]}
-            />
-            <ProForm.Group title="节点" size={8}>
-              <ProFormSelect
-                width="sm"
-                name="source"
-                placeholder="选择来源节点"
-              />
-              <ProFormSelect
-                width="sm"
-                name="target"
-                placeholder="选择目标节点"
-              />
-            </ProForm.Group>
-          </ProCard>
-
-          <ProCard
-            title="顶部对齐"
-            bordered
-            headerBordered
-            collapsible
-            style={{
-              minWidth: 800,
-              marginBlockEnd: 16,
-            }}
-          >
-            <ProFormDigit
-              name="xs"
-              label="XS号表单"
-              initialValue={9999}
-              tooltip="悬浮出现的气泡。"
-              placeholder="请输入名称"
-              width="xs"
-            />
-            <ProFormText
-              name="s"
-              label="S号表单"
-              placeholder="请输入名称"
-              width="sm"
-            />
-            <ProFormDateRangePicker name="m" label="M 号表单" />
-            <ProFormSelect
-              name="l"
-              label="L 号表单"
-              fieldProps={{
-                mode: 'tags',
+            <ProCard
+              title="Excel上传"
+              bordered
+              headerBordered
+              collapsible
+              style={{
+                marginBlockEnd: 16,
+                minWidth: 800,
+                maxWidth: '100%',
               }}
-              width="lg"
-              initialValue={['吴家豪', '周星星']}
-              options={['吴家豪', '周星星', '陈拉风'].map((item) => ({
-                label: item,
-                value: item,
-              }))}
-            />
-          </ProCard>
-        </StepsForm.StepForm>
-        <StepsForm.StepForm name="checkbox" title="第二步骤">
-          <ProCard
-            style={{
-              minWidth: 800,
-              marginBlockEnd: 16,
-              maxWidth: '100%',
-            }}
-          >
-            <ProFormCheckbox.Group
-              name="checkbox"
-              label="迁移类型"
-              width="lg"
-              options={['结构迁移', '全量迁移', '增量迁移', '全量校验']}
-            />
-            <ProForm.Group>
-              <ProFormText name="dbname" label="业务 DB 用户名" />
-              <ProFormDatePicker
-                name="datetime"
-                label="记录保存时间"
-                width="sm"
+            >
+              <Dragger {...props}>
+                <p className="ant-upload-drag-icon">
+                  <InboxOutlined />
+                </p>
+                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                <p className="ant-upload-hint">
+                  Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+                  banned files.
+                </p>
+              </Dragger>
+            </ProCard>
+          </StepsForm.StepForm>
+          <StepsForm.StepForm name="checkbox" title="第二步骤">
+            <ProCard
+              style={{
+                minWidth: 800,
+                marginBlockEnd: 16,
+                maxWidth: '100%',
+              }}
+            >
+              <ProFormCheckbox.Group
+                name="checkbox"
+                label="迁移类型"
+                width="lg"
+                options={['结构迁移', '全量迁移', '增量迁移', '全量校验']}
               />
-            </ProForm.Group>
-            <ProFormCheckbox.Group
-              name="checkbox"
-              label="迁移类型"
-              options={['完整 LOB', '不同步 LOB', '受限制 LOB']}
-            />
-          </ProCard>
-        </StepsForm.StepForm>
-        <StepsForm.StepForm name="time" title="第三步骤">
-          <ProCard
-            style={{
-              marginBlockEnd: 16,
-              minWidth: 800,
-              maxWidth: '100%',
-            }}
-          >
-            <ProFormCheckbox.Group
-              name="checkbox"
-              label="部署单元"
-              rules={[
-                {
-                  required: true,
-                },
-              ]}
-              options={['部署单元1', '部署单元2', '部署单元3']}
-            />
-            <ProFormSelect
-              label="部署分组策略"
-              name="remark"
-              rules={[
-                {
-                  required: true,
-                },
-              ]}
-              width="md"
-              initialValue="1"
-              options={[
-                {
-                  value: '1',
-                  label: '策略一',
-                },
-                { value: '2', label: '策略二' },
-              ]}
-            />
-            <ProFormSelect
-              label="Pod 调度策略"
-              name="remark2"
-              width="md"
-              initialValue="2"
-              options={[
-                {
-                  value: '1',
-                  label: '策略一',
-                },
-                { value: '2', label: '策略二' },
-              ]}
-            />
-          </ProCard>
-        </StepsForm.StepForm>
-      </StepsForm>
-    </>
-   
+              <ProForm.Group>
+                <ProFormText name="dbname" label="业务 DB 用户名" />
+                <ProFormDatePicker
+                  name="datetime"
+                  label="记录保存时间"
+                  width="sm"
+                />
+              </ProForm.Group>
+              <ProFormCheckbox.Group
+                name="checkbox"
+                label="迁移类型"
+                options={['完整 LOB', '不同步 LOB', '受限制 LOB']}
+              />
+            </ProCard>
+          </StepsForm.StepForm>
+          <StepsForm.StepForm name="time" title="第三步骤">
+            <ProCard
+              style={{
+                marginBlockEnd: 16,
+                minWidth: 800,
+                maxWidth: '100%',
+              }}
+            >
+              <ProFormCheckbox.Group
+                name="checkbox"
+                label="部署单元"
+                rules={[
+                  {
+                    required: true,
+                  },
+                ]}
+                options={['部署单元1', '部署单元2', '部署单元3']}
+              />
+              <ProFormSelect
+                label="部署分组策略"
+                name="remark"
+                rules={[
+                  {
+                    required: true,
+                  },
+                ]}
+                width="md"
+                initialValue="1"
+                options={[
+                  {
+                    value: '1',
+                    label: '策略一',
+                  },
+                  { value: '2', label: '策略二' },
+                ]}
+              />
+              <ProFormSelect
+                label="Pod 调度策略"
+                name="remark2"
+                width="md"
+                initialValue="2"
+                options={[
+                  {
+                    value: '1',
+                    label: '策略一',
+                  },
+                  { value: '2', label: '策略二' },
+                ]}
+              />
+            </ProCard>
+          </StepsForm.StepForm>
+        </StepsForm>
+      </>
+
     </PageContainer>
   );
 };
